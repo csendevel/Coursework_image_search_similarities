9ea0f8d021634532b7aaea8bd04c1bb8df606dd6
Valeriy <43117310+csendevel@users.noreply.github.com>
2021-07-14 22:40:07 +0300

    Add histogram visualization

--- Hist_SSIM_compare.py
+++ Hist_SSIM_compare.py
@@ -2,6 +2,7 @@ import random
 import math
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import requests
 import os
 import time
@@ -38,14 +39,36 @@ def compare(image_name1, image_name2):
     hist_2 = cv2.calcHist([image2], [0,1,2], None, [16,16,16],[0,256,0,256,0,256])
     hist_2 = cv2.normalize(hist_2, hist_2).flatten()
 
+    images = [image1, image2]
 
-    similarity=cv2.compareHist(hist_1, hist_2, cv2.HISTCMP_INTERSECT)
-    normal_sim=cv2.compareHist(hist_1, hist_1, cv2.HISTCMP_INTERSECT)
+    plt.figure(figsize = (18, 5))
+    
+    for i in range(2):
+        plt.subplot(1, 2, i + 1)
+
+        color = ('b','g','r')
+        for j,col in enumerate(color):
+            hist = cv2.calcHist([images[i]],[j], None,[256],[0,256])
+            plt.plot(hist, color = col)
+            plt.xlim([0,256])
+
+    plt.show()
+
+    similarity = cv2.compareHist(hist_1, hist_2, cv2.HISTCMP_INTERSECT)
+    normal_sim = cv2.compareHist(hist_1, hist_1, cv2.HISTCMP_INTERSECT)
 
     print("Normal similarity: ", normal_sim)
     print("Hist similarity: ", similarity)
     print("SSIM: ", mssim)
 
+    print(similarity / normal_sim * 100)
+
+
 # compare("2.1.jpg", "2.2.jpg")
-# compare("1.1.jpg", "1.2.jpg")
-# compare("5.jpg", "6.jpg")
+#compare("1.1.jpg", "1.2.jpg")
+
+#same
+#compare("5.jpg", "6.jpg")
+
+#different
+#compare("01.jpg", "1.1.jpg")
